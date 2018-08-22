b80f4b3b89c59899ecb93246b40f3005c6a50e12
huker <huaolie@sina.com>
2018-08-22 17:34:02 +0800

    add center list

--- src/container/Center/Center.js
+++ src/container/Center/Center.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { List } from 'antd-mobile-rn';
+const Item = List.Item;
 
 export default class Center extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
     render() {
         return (
             <View>
-                <Text>Center</Text>
+                <List style={{ marginTop: 10 }}>
+                    <Item arrow="horizontal" onClick={() => {
+                    }}>
+                        我
+                    </Item>
+                    <Item arrow="horizontal" onClick={() => {
+                    }}>
+                        历史记录
+                    </Item>
+
+                </List>
+                <List style={{ marginTop: 10 }}>
+                    <Item arrow="horizontal" onClick={() => {
+                    }}>
+                        设置
+                    </Item>
+                </List>
             </View>
         );
     }
