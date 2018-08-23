d9d61565334eb92f516724278695b7fafb3f13d0
huker <huaolie@sina.com>
2018-08-23 14:18:10 +0800

    update readme

--- src/container/index.js
+++ src/container/index.js
@@ -5,13 +5,11 @@ import { Navigation } from 'react-native-navigation';
 import Center from "./Center/Center";
 import Home from "./Home/Home";
 import Login from "./Login/Login";
-import Loading from "./Loading/Loading";
 import Scan from "./Scan/Scan";
 
 export function registerTabs(store, Provider) {
     Navigation.registerComponent('home', () => Home, store, Provider);
     Navigation.registerComponent('center', () => Center, store, Provider);
     Navigation.registerComponent('login', () => Login, store, Provider);
-    Navigation.registerComponent('loading', () => Loading, store, Provider);
     Navigation.registerComponent('scan', () => Scan, store, Provider);
 }
